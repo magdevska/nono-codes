4ec4d55a4db4e78a9ca7f9dff5b5268d8573ca20
Lidija Magdevska <lidija.magdevska@gmail.com>
2024-08-24 10:41:54 +0200

    Partition choices added.

--- melting_temperature.py
+++ melting_temperature.py
@@ -6,6 +6,7 @@ from concurrent.futures.process import ProcessPoolExecutor
 from dataclasses import dataclass
 from textwrap import dedent
 from typing import Any, Self
+import itertools
 
 from Bio.SeqUtils import MeltingTemp
 
@@ -18,6 +19,7 @@ class PrecomputationData:
     left: list[list[str]]
     right: list[list[str]]
     storage_depth: int
+    partition_choice: int
 
     @classmethod
     def _compute_all(cls, left: list[int], right: list[int], i: int) -> int:
@@ -27,19 +29,54 @@ class PrecomputationData:
         return s
 
     @classmethod
-    def precompute(cls, codeword_length: int, *, do_print: bool = False) -> Self:
+    def generate_pre_suf_fixes(cls, left: list[list[str]], right: list[list[str]], i: int) -> list[str]:
+        span_end = i - 1
+
+        result: list[str] = []
+        for j in range(i):
+            result.extend(f"{left}{right}" for left, right in itertools.product(left[j], right[span_end - j]))
+
+        return result
+
+    @classmethod
+    def precompute(cls, codeword_length: int, partition_choice: int, *, do_print: bool = False) -> Self:
         if codeword_length < 23:
             left_partition_sizes = [3, 3]
             right_partition_sizes = [1, 0]
             storage_depth = 2
-            left = [["C", "G", "A"], ["CT", "GT", "AT"]]
-            right = [["T"], []]
+
+            if partition_choice <= 0 or partition_choice > 3:
+                left = [["C", "G", "A"], ["CT", "GT", "AT"]]
+                right = [["T"], []]
+            elif partition_choice == 1:
+                left = [["C", "G", "T"], ["CA", "GA", "TA"]]
+                right = [["A"], []]
+            elif partition_choice == 2:
+                left = [["C", "T", "A"], ["CG", "TG", "AG"]]
+                right = [["G"], []]
+            else:
+                left = [["G", "T", "A"], ["GC", "TC", "AC"]]
+                right = [["C"], []]
         else:
             left_partition_sizes = [3, 3, 0, 1]
             right_partition_sizes = [1, 0, 3, 8]
             storage_depth = 4
-            left = [["C", "G", "A"], ["CT", "GT", "AT"], [], ["CCTT"]]
-            right = [["T"], [], ["CTT", "GTT", "ATT"], ["CGTT", "CATT", "GCTT", "GGTT", "GATT", "ACTT", "AGTT", "AATT"]]
+            if partition_choice < 9 or partition_choice > 35:
+                left = [["A", "C", "G"], ["CT", "GT", "AT"], [], []]
+                right = [["T"], [], ["ATT", "CTT", "GTT"]]
+            elif partition_choice < 18:
+                left = [["C", "G", "T"], ["CA", "GA", "TA"], [], []]
+                right = [["A"], [], ["CAA", "GAA", "TAA"]]
+            elif partition_choice < 27:
+                left = [["C", "T", "A"], ["CG", "TG", "AG"], [], []]
+                right = [["G"], [], ["CGG", "TGG", "AGG"]]
+            else:
+                left = [["G", "T", "A"], ["GC", "TC", "AC"], [], []]
+                right = [["C"], [], ["GCC", "TCC", "ACC"]]
+
+            words = cls.generate_pre_suf_fixes(left, right, 3)
+            left[3] = words[partition_choice % 9]
+            right.append(words[0 : (partition_choice % 9)] + words[partition_choice % 9 + 1 :])
 
         for i in range(storage_depth, codeword_length):
             left_partition_sizes.append(0)
@@ -57,17 +94,18 @@ class PrecomputationData:
             left=left,
             right=right,
             storage_depth=storage_depth,
+            partition_choice=partition_choice,
         )
 
 
 def find_codeword(
-        left_partition_sizes: list[int],
-        right_partition_sizes: list[int],
-        left: list[list[str]],
-        right: list[list[str]],
-        i: int,
-        k: int,
-        storage_depth: int,
+    left_partition_sizes: list[int],
+    right_partition_sizes: list[int],
+    left: list[list[str]],
+    right: list[list[str]],
+    i: int,
+    k: int,
+    storage_depth: int,
 ) -> str:
     if i < storage_depth:
         return right[i][k]
@@ -99,9 +137,11 @@ def chunked_range(range_min: int, range_max: int, num_chunks: int) -> list[range
 
 
 # returns increment of adequate_melting
-def batched_parallel_unit_of_work(codeword_length: int, unit_of_work_offsets: range) -> list[int]:
+def batched_parallel_unit_of_work(
+    codeword_length: int, partition_choice: int, unit_of_work_offsets: range
+) -> list[int]:
     # every process precomputes this as it takes negligible time
-    data = PrecomputationData.precompute(codeword_length)
+    data = PrecomputationData.precompute(codeword_length, partition_choice)
 
     result = [0, 0, 0, 0, 0, 0, 0, 0]
     for uow_offset in unit_of_work_offsets:
@@ -140,6 +180,7 @@ def main() -> None:
     parser.add_argument("--codeword-length", type=int, required=True)
     parser.add_argument("--node-index", type=int, required=False, default=0)
     parser.add_argument("--node-count", type=int, required=False, default=1)
+    parser.add_argument("--partition-choice", type=int, required=False, default=0)
     args = parser.parse_args()
 
     if args.codeword_length < 10:
@@ -150,7 +191,7 @@ def main() -> None:
         exit(2)
 
     process_count = multiprocessing.cpu_count()
-    data = PrecomputationData.precompute(args.codeword_length)
+    data = PrecomputationData.precompute(args.codeword_length, args.partition_choice)
 
     per_node_chunks = chunked_range(0, data.right_partition_sizes[data.codeword_length - 1], num_chunks=args.node_count)
     my_node_chunk = per_node_chunks[args.node_index]
@@ -174,12 +215,13 @@ def main() -> None:
     time_start = time.time()
     futures: list[Future] = []
     for chunk in per_process_chunks:
-        futures.append(pool.submit(batched_parallel_unit_of_work, data.codeword_length, chunk))
+        futures.append(pool.submit(batched_parallel_unit_of_work, data.codeword_length, data.partition_choice, chunk))
     num_of_adequate_melting_temps = [sum(f.result()[i] for f in futures) for i in range(8)]
     time_end = time.time()
 
     print(
-        f"### RESULT ### num of adequate melting temps (node {args.node_index} of {args.node_count}): {num_of_adequate_melting_temps !r}"
+        f"### RESULT ### num of adequate melting temps per range (node {args.node_index} of {args.node_count}): {num_of_adequate_melting_temps !r} \n"
+        f"total number of adequate melting temps in partition # {args.partition_choice}: {sum(num_of_adequate_melting_temps[0:7])}"
     )
     print(f"time taken (node {args.node_index} of {args.node_count}): {int(time_end - time_start)} seconds")
 
